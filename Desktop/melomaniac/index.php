c0e31be11a5dc7f4721d53f5fba07265e1e69094
SeannFuerte <seannfuerte7@gmail.com>
2024-03-12 12:45:16 +0800

    index.php

---
+++ Desktop/melomaniac/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+    <title>Song 2</title>
+    <style>
+        a
+        {
+        color:black;
+        text-decoration: none;
+        font-size: 25px;
+        }
+    </style>
+
+<head>
+<body style = "text-align:center;">
+    <h1>HighSchool LSS</h1>
+    <a href = song1.php> Pusong Bato</a><br>
+    <a href = song2.php> Classmate</a><br>
+    <a href = song3.php> Highschool Life</a><br>
+    <a href = song4.php> Thinking Out Loud </a><br>
+    <a href = song5.php> Hotel California</a><br>
+    <a href = song6.php> Photograph</a><br>
+
+</body>
